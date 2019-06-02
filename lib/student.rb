f8de72644bd8de0db4a3ccbbe07e04c3dca46441
Sarah Williams <srwilliams8678@gmail.com>
2019-06-02 20:50:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,14 +3,14 @@ class Student < User
   attr_accessor :knowledge
 
   def initialize
-  @knowledge = []
-end
+    @knowledge = []
+  end
 
-def learn(knowledge)
-  @knowledge << knowledge
-end
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
-def knowledge
-  @knowledge
-end
+  def knowledge
+    @knowledge
+  end
 end
